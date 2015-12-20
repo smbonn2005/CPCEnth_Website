13ca0bfb0253b5070918300052c4eef7749de73a
Stephen Bonnell <stephen@smbonn.me>
2015-12-19 21:47:53 -0500

    login multiple users #3

--- new_site/login.php
+++ new_site/login.php
@@ -8,7 +8,7 @@ if(!$username || !$password){
 
 $auth_username = array("admin","smbonn2005");
 $auth_password = array("adminpassword123","potter");
-$key = array_search(strtolower($username), $auth_username)
+$key = array_search(strtolower($username), $auth_username);
 if($key !== false && $password == $auth_password[$key]){
 	// Good login
 	setcookie('username',$username,time()+7*24*60*60);
