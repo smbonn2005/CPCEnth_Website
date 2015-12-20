7f81f7e0b6ada738fa634452c050d4a907eb9e1c
Stephen Bonnell <stephen@smbonn.me>
2015-12-19 21:13:37 -0500

    admin login page

---
+++ new_site/login.php
@@ -0,0 +1,20 @@
+<?
+$username = (isset($_POST['username']) && !empty($_POST['username'])) ? $_POST['username'] : false;
+$password = (isset($_POST['password']) && !empty($_POST['password'])) ? base64_decode($_POST['password']) : false;
+
+if(!$username || !$password){
+	die(false);
+}
+
+$auth_username = "admin";
+$auth_password = "adminpassword123";
+
+if(strtolower($username) == $auth_username && $password = $auth_password){
+	// Good login
+	setcookie('username',$username,time()+7*24*60*60);
+	die(true);
+} else {
+	// Incorrect Login
+	die(false);
+}
+?>
